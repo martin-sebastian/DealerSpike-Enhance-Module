e9e7ab58d04ed1bb6bb223dbd5d51ddbd28c93a5
Martin Sebastian <martin-sebastian@users.noreply.github.com>
2024-07-30 17:05:52 -0400

    Routing
    routing

--- playground/movies.js
+++
@@ -1,7 +0,0 @@
-async function logMovies() {
-  const response = await fetch("http://example.com/movies.json");
-  const movies = await response.json();
-  console.log(movies);
-}
-
-return logMovies();
